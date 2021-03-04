c8ba8823b884c87f89b5d921a2459a97d62d8450
Matthew Healy <matthew.healy@soundcloud.com>
2021-03-04 12:32:47 +0100

    Add empty vm implementation

--- src/main.c
+++ src/main.c
@@ -1,8 +1,10 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, const char* argv[]) {
+    init_vm();
     Chunk chunk;
     init_chunk(&chunk);
 
@@ -14,6 +16,8 @@ int main(int argc, const char* argv[]) {
 
     disassemble_chunk(&chunk, "test chunk");
 
+    free_vm();
     free_chunk(&chunk);
+
     return 0;
 }
---
+++ src/vm.c
@@ -0,0 +1,8 @@
+#include "common.h"
+#include "vm.h"
+
+VM vm;
+
+void init_vm() {}
+
+void free_vm() {}
