b6d50d02d668370aa218260ac010afed6427f034
Matthew Healy <matthew.healy@soundcloud.com>
2021-03-11 17:42:58 +0100

    Handle single and double char tokens

--- src/scanner.c
+++ src/scanner.c
@@ -42,8 +42,82 @@ static Token error_token(const char *message) {
     return token;
 }
 
+static char advance() {
+    scanner.current++;
+    return scanner.current[-1];
+}
+
+static bool match(char expected) {
+    if (is_at_end()) return false;
+    if (*scanner.current != expected) return false;
+
+    scanner.current++;
+    return true;
+}
+
+static char peek() {
+    return *scanner.current;
+}
+
+static void skip_whitespace() {
+    for (;;) {
+        char c = peek();
+        switch (c) {
+            case ' ':
+            case '\r':
+            case '\t':
+                advance();
+                break;
+            case '\n':
+                scanner.line++;
+                advance();
+                break;
+            default: return;
+        }
+    }
+}
+
 Token scan_token() {
+    skip_whitespace();
+
     scanner.start = scanner.current;
     if (is_at_end()) return make_token(TOKEN_EOF);
+
+    char c = advance();
+
+    switch (c) {
+        case '(': return make_token(TOKEN_LEFT_PAREN);
+        case ')': return make_token(TOKEN_RIGHT_PAREN);
+        case '{': return make_token(TOKEN_LEFT_BRACE);
+        case '}': return make_token(TOKEN_RIGHT_PAREN);
+        case ';': return make_token(TOKEN_SEMICOLON);
+        case ',': return make_token(TOKEN_COMMA);
+        case '.': return make_token(TOKEN_DOT);
+        case '-': return make_token(TOKEN_MINUS);
+        case '+': return make_token(TOKEN_PLUS);
+        case '/': return make_token(TOKEN_SLASH);
+        case '*': return make_token(TOKEN_STAR);
+        case '!':
+            return make_token(match('=')
+                ? TOKEN_BANG_EQUAL
+                : TOKEN_BANG
+            );
+        case '=':
+            return make_token(match('=')
+                ? TOKEN_EQUAL_EQUAL
+                : TOKEN_EQUAL
+            );
+        case '<':
+            return make_token(match('=')
+                ? TOKEN_LESS_EQUAL
+                : TOKEN_LESS
+            );
+        case '>':
+            return make_token(match('=')
+                ? TOKEN_GREATER_EQUAL
+                : TOKEN_GREATER
+            );
+    }
+
     return error_token("Unexpected character.");
 }
