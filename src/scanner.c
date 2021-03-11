e10028fac4d40dda8c135b4daf402fde4ee08ff2
Matthew Healy <matthew.healy@soundcloud.com>
2021-03-11 17:05:00 +0100

    Plumb in scanner for tokenisation

---
+++ src/scanner.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "common.h"
+#include "scanner.h"
+
+typedef struct {
+    const char *start;
+    const char *current;
+    int line;
+} Scanner;
+
+Scanner scanner;
+
+void init_scanner(const char *source) {
+    scanner.start = source;
+    scanner.current = source;
+    scanner.line = 1;
+}
+
+static bool is_at_end() {
+    return *scanner.current == '\0';
+}
+
+static Token make_token(TokenType type) {
+    Token token;
+    token.type = type;
+    token.start = scanner.start;
+    token.length = (int)(scanner.current - scanner.start);
+    token.line = scanner.line;
+
+    return token;
+}
+
+static Token error_token(const char *message) {
+    Token token;
+    token.type = TOKEN_ERROR;
+    token.start = message;
+    token.length = (int)strlen(message);
+    token.line = scanner.line;
+
+    return token;
+}
+
+Token scan_token() {
+    scanner.start = scanner.current;
+    if (is_at_end()) return make_token(TOKEN_EOF);
+    return error_token("Unexpected character.");
+}
